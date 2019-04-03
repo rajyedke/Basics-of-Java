2b9493758b75fefc625d7af34f802493dbdf39da
rajyedke <rajyedke10@gmail.com>
2019-04-04 00:05:00 +0530

    Update Character having Largest ASCII value in a String.java

--- Character having Largest ASCII value in a String.java
+++ Character having Largest ASCII value in a String.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 public class Solution{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("enter");
+		
+		System.out.println("enter the string");
 		Scanner sc=new Scanner(System.in);
 		String st=sc.next();
-				char large='0';
+		char large='0';
 		for(int i=0;i<st.length();i++)
-			
 			if((int)st.charAt(i)>(int)large)
 				large=st.charAt(i);
 		
-				System.out.println(large);
+		System.out.println("Character having Largest ASCII value is "+large);
 	}
 	
 }
